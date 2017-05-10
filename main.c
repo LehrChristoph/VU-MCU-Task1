e32f2c1316038b57363557d9724ad3e6edb104d9
Unknown <christoph.lehr@gmx.at>
2017-05-10 10:10:53 +0200

    Updated ZigBee Control and small changes in FFT as well as Noise Reduction

--- main.c
+++ main.c
@@ -33,6 +33,8 @@
 
 /* prototypes */
 static void init(void);
+static void drawFFT(void);
+static void drawInfoScreen(void);
 static void zigBeeCB(uint16_t src_addr, const char *data, uint8_t data_length, uint8_t broadcast);
 
 
@@ -41,10 +43,11 @@ static void zigBeeCB(uint16_t src_addr, const char *data, uint8_t data_length, u
 #define ADC_MODE_SAMPLE 2
 #define ADC_MODE_START_FFT 4
 
-#define TIMER1_COUNTER_TOP_PWM 640
-//#define TIMER1_COUNTER_TOP_PWM 2950
+//#define TIMER1_COUNTER_TOP_PWM 640
+#define TIMER1_COUNTER_TOP_PWM 2950
 //#define TIMER1_COUNTER_TOP_PWM 1475
-#define MAX_RPM 3600
+#define MAX_RPM 3400
+#define MIN_RPM 650
 #define TIMER3_COUNTER_TOP_A 250
 #define TIMER3_COUNTER_TOP_B 40000
 #define TIMER5_COUNTER_TOP 25000
@@ -64,9 +67,9 @@ static void zigBeeCB(uint16_t src_addr, const char *data, uint8_t data_length, u
 #define MODE_USE_ZIGBEE 2
 #define MODE_NOISE_REDUCTION 4
 
-#define PID_P_SHARE 5
+#define PID_P_SHARE 100
 #define PID_I_SHARE 5
-#define PID_D_SHARE 5
+#define PID_D_SHARE 0.0004
 
 #define SERIALNET_ROLE 2
 #define SERIALNET_ADDR 65535
@@ -89,6 +92,7 @@ static complex_t fft_buffter[FFT_N];		/* FFT buffer */
 static uint16_t fft_spectrum[FFT_N/2];		/* Spectrum output buffer */
 static int16_t fft_capture[FFT_N];
 static volatile uint8_t fft_counter=0;
+static char errorLast[30]="";
 
 int main(void)
 {
@@ -129,7 +133,7 @@ int main(void)
             if(median != median_temp[2]){
                 
                 if(source_mode == MODE_USE_ADC){
-                    OCR1B = median_temp[2]*2.5;
+                    OCR1B = median_temp[2]*(TIMER1_COUNTER_TOP_PWM/255);
                     set_rpm = 10.7843*median_temp[2]+650;
                 }
                 median = median_temp[2];
@@ -152,6 +156,10 @@ int main(void)
         
         if((isr_flags & ISR_NOISE_REDUCTION) == ISR_NOISE_REDUCTION){
             source_mode = MODE_USE_ADC;
+            
+            OCR1B = median*(TIMER1_COUNTER_TOP_PWM/255);
+            set_rpm = 10.7843*median+650;
+            
             isr_flags &= ~(ISR_NOISE_REDUCTION);
         }
         
@@ -160,7 +168,15 @@ int main(void)
             isr_flags &= ~(ISR_INPUT_CAPTURE);
                         
             measured_rpm = 15 / (ICR4 *0.000008);
+            
+            if(sum_up > 300 && source_mode == MODE_NOISE_REDUCTION){
                 
+                set_rpm -= (set_rpm/10);
+                if(set_rpm < MIN_RPM){
+                    set_rpm =MIN_RPM;
+                }
+            }
+            /*
             if(set_rpm > measured_rpm){
                 
                 uint16_t variance = set_rpm - measured_rpm;
@@ -202,9 +218,9 @@ int main(void)
                 }else{
                     OCR1B =0;
                 }
-            }
+            }*/
             
-            /*
+            
             int16_t error= (int16_t)set_rpm - (int16_t)measured_rpm;
             int16_t p_share=PID_P_SHARE * error;
             int16_t i_share=PID_I_SHARE * error_priveous;
@@ -214,43 +230,8 @@ int main(void)
             int16_t pid = p_share + i_share + d_share;
             
             OCR1B += pid *(TIMER1_COUNTER_TOP_PWM/MAX_RPM);
-            */
-            
-            if(displayMode == GLCD_DISPLAY_RPM){
-                
-                char line1[30]="";
-                char line2[30]="";
-                xy_point line_set_rpm, line_input_capture_rpm, rect_clear_left, rect_clear_right;
             
             
-                line_set_rpm.x = 3;
-                line_set_rpm.y = 8;
-                
-                line_input_capture_rpm.x=3;
-                line_input_capture_rpm.y=18;
-                
-                rect_clear_left.x = 60;
-                rect_clear_left.y = 0;
-                
-                rect_clear_right.x=100;
-                rect_clear_right.y=24;
-                
-                if(set_rpm < 1000){
-                    sprintf(line1, "Set:       %d RPM", set_rpm);
-                }else{
-                    sprintf(line1, "Set:      %d RPM", set_rpm);
-                }
-                
-                if(measured_rpm < 1000){
-                    sprintf(line2, "Measured:  %d RPM", measured_rpm);
-                }else{
-                    sprintf(line2, "Measured: %d RPM", measured_rpm);
-                }
-                
-                glcdFillRect(rect_clear_left, rect_clear_right, &glcdClearPixel);
-                glcdDrawText(line1, line_set_rpm, &Standard5x7, &glcdSetPixel);
-                glcdDrawText(line2, line_input_capture_rpm, &Standard5x7, &glcdSetPixel);
-            }
         }
         
         if((isr_flags & ISR_FFT) == ISR_FFT){
@@ -263,6 +244,9 @@ int main(void)
             fft_execute(fft_buffter);
             //get result of fft
             fft_output(fft_buffter, fft_spectrum);
+            
+            adc_mode = ADC_MODE_RPM;
+            TIMSK3|= 1<<OCIE3A;
             
             uint16_t max_specs[8] = {fft_spectrum[2],fft_spectrum[3],fft_spectrum[4],fft_spectrum[5],fft_spectrum[6],fft_spectrum[7],fft_spectrum[7],fft_spectrum[9]};
             //uint16_t sum_up=0;
@@ -287,44 +271,41 @@ int main(void)
             }
             
 
-            if((sum_up>>5)  > 400+0.048828125*measured_rpm){
+            if((sum_up>>5)  > 400+0.048828125*measured_rpm && source_mode != MODE_NOISE_REDUCTION){
                 
                 PORTB |=0x08;
                 
-                if(source_mode == MODE_NOISE_REDUCTION){
-                    set_rpm-=(set_rpm /10);
-                }else{
+                set_rpm = set_rpm/2;
+                
+                if(set_rpm < MIN_RPM){
+                    set_rpm = MIN_RPM;
+                }
+                
+                if(source_mode == MODE_USE_ZIGBEE){
+                    uint8_t i=0;
+                    serialnet_broadcast_data("NOISEALERT");
+                    serialnet_dispatch();
+                    
+                    while((serialnet_get_state() & SERIALNET_RESPONSE_ERR) == SERIALNET_RESPONSE_ERR && i < 5){
+                        serialnet_broadcast_data("NOISEALERT");
+                        serialnet_dispatch();
+                        i++;
+                    }
+                    
+                    if((serialnet_get_state() & SERIALNET_RESPONSE_ERR) == SERIALNET_RESPONSE_ERR ){
+                        sprintf(errorLast, "ERROR SEND NOISEALERT");
+                    }
+                    
+                    serialnet_clear_state(SERIALNET_RESPONSE_OK);
+                    serialnet_clear_state(SERIALNET_RESPONSE_ERR);
+                    
                     
                 }
-                set_rpm = set_rpm/2;
+                
                 source_mode = MODE_NOISE_REDUCTION;
                 
             }
-            
-            
-            if((displayMode & GLCD_DISPLAY_FFT) == GLCD_DISPLAY_FFT){
-                glcdFillScreen(GLCD_CLEAR);
-                
-                for(int i=0; i<FFT_N/2;i++){
-                    xy_point top, bottom;
-                    
-                    top.x=i*2;
-                    if(fft_spectrum[i]>=63){
-                        top.y=0;
-                    }else{
-                        top.y=63-fft_spectrum[i];
-                    }
-                    
-                    bottom.x=i*2+1;
-                    bottom.y=63;
-                    
-                    glcdFillRect(top, bottom, &glcdSetPixel);
-                }   
-            }
-            
-            adc_mode = ADC_MODE_RPM;
-            TIMSK3|= 1<<OCIE3A;
-            
+
         }   
         
         if((isr_flags & ISR_REFRESH_ZIGBEE) == ISR_REFRESH_ZIGBEE){
@@ -332,6 +313,15 @@ int main(void)
             PORTC = serialnet_get_state();
             
             isr_flags &= ~(ISR_REFRESH_ZIGBEE);
+            
+            if((displayMode & GLCD_DISPLAY_FFT) == GLCD_DISPLAY_FFT){
+                drawFFT();
+            }
+            
+            if(displayMode == GLCD_DISPLAY_RPM){
+                drawInfoScreen();
+                
+            }
         }
         
     }
@@ -414,9 +404,15 @@ static void zigBeeCB(uint16_t src_addr, const char *data, uint8_t data_length, u
         PORTB |= 0x01;
         
         zigbee_rpm = atoi(&data[7]);
-        set_rpm = zigbee_rpm;
-        source_mode = MODE_USE_ZIGBEE;
-
+        if(zigbee_rpm < 650 || zigbee_rpm > 3400){
+            zigbee_rpm =0;
+        }else{
+            set_rpm = zigbee_rpm;
+        }
+        
+        if(source_mode != MODE_NOISE_REDUCTION){
+            source_mode = MODE_USE_ZIGBEE;
+        }
     }else if(strncmp("RESETALERT", data, 9)==0){
         PORTB |= 0x02;
         source_mode = MODE_USE_ZIGBEE;
@@ -424,15 +420,120 @@ static void zigBeeCB(uint16_t src_addr, const char *data, uint8_t data_length, u
         
     }else if(strncmp("RESETRPM", data, 8)==0){
         PORTB |= 0x04;
-        
-        source_mode = MODE_USE_ADC;
-        OCR1B = median*2.5;
-        set_rpm = 10.7843*median+650;
+        if(source_mode == MODE_USE_ZIGBEE){
+            source_mode = MODE_USE_ADC;
+            OCR1B = median*(TIMER1_COUNTER_TOP_PWM/255);
+            set_rpm = 10.7843*median+650;
+        }
+        zigbee_rpm=0;
     }
     
     serialnet_clear_state(SERIALNET_GOT_DATA);
 }
 
+static void drawFFT(void){
+    glcdFillScreen(GLCD_CLEAR);
+                
+    for(int i=0; i<FFT_N/2;i++){
+        xy_point top, bottom;
+        
+        top.x=i*2;
+        if((fft_spectrum[i]/2)>=63){
+            top.y=0;
+        }else{
+            top.y=63-(fft_spectrum[i]/2);
+        }
+        
+        bottom.x=i*2+1;
+        bottom.y=63;
+        
+        glcdFillRect(top, bottom, &glcdSetPixel);
+    }   
+}
+
+static void drawInfoScreen(void){
+    char drawLine[30]="";
+    xy_point line_xy, rect_clear_left, rect_clear_right;
+
+
+    line_xy.x = 3;
+    line_xy.y = 8;
+    
+    rect_clear_left.x = 90;
+    rect_clear_left.y = 0;
+    
+    rect_clear_right.x=128;
+    rect_clear_right.y=10;
+    
+    if(set_rpm < 1000){
+        sprintf(drawLine, "Set RPM:         %d", set_rpm);
+    }else{
+        sprintf(drawLine, "Set RPM:        %d", set_rpm);
+    }
+    
+
+    
+    glcdFillRect(rect_clear_left, rect_clear_right, &glcdClearPixel);
+    glcdDrawText(drawLine, line_xy, &Standard5x7, &glcdSetPixel);
+    
+    line_xy.y=line_xy.y+10;
+    
+    rect_clear_left.y = rect_clear_left.y + 10;
+    
+    rect_clear_right.y=rect_clear_right.y + 10;
+    
+    if(measured_rpm < 1000){
+        sprintf(drawLine, "Measured RPM:    %d", measured_rpm);
+    }else{
+        sprintf(drawLine, "Measured RPM:   %d", measured_rpm);
+    }
+    
+    glcdFillRect(rect_clear_left, rect_clear_right, &glcdClearPixel);
+    glcdDrawText(drawLine, line_xy, &Standard5x7, &glcdSetPixel);
+    
+    line_xy.y=line_xy.y+10;
+    
+    rect_clear_left.y = rect_clear_left.y + 10;
+    
+    rect_clear_right.y=rect_clear_right.y + 10;
+    
+    if(source_mode == MODE_NOISE_REDUCTION){
+        sprintf(drawLine, "Noise reduction:  ON");
+    }else{
+        sprintf(drawLine, "Noise reduction: OFF");
+    }                     
+    
+    glcdFillRect(rect_clear_left, rect_clear_right, &glcdClearPixel);
+    glcdDrawText(drawLine, line_xy, &Standard5x7, &glcdSetPixel);
+    
+    line_xy.y=line_xy.y+10;
+    
+    rect_clear_left.x= 64;
+    rect_clear_left.y = rect_clear_left.y + 10;
+    
+    rect_clear_right.y=rect_clear_right.y + 10;
+    
+    if(zigbee_rpm != 0){
+        sprintf(drawLine, "Source:    SerialNet");
+    }else{
+        sprintf(drawLine, "Source:         ADC0");
+    }
+    
+    glcdFillRect(rect_clear_left, rect_clear_right, &glcdClearPixel);
+    glcdDrawText(drawLine, line_xy, &Standard5x7, &glcdSetPixel);
+    
+    line_xy.y=line_xy.y+10;
+    
+    rect_clear_left.x= 0;
+    rect_clear_left.y = rect_clear_left.y + 10;
+    
+    rect_clear_right.y=rect_clear_right.y + 10;
+    
+    glcdFillRect(rect_clear_left, rect_clear_right, &glcdClearPixel);
+    glcdDrawText(errorLast, line_xy, &Standard5x7, &glcdSetPixel);
+    
+    
+}
 
 
 /* interrupts */
@@ -497,6 +598,8 @@ ISR(INT0_vect){
 }
 
 ISR(INT1_vect){
-    isr_flags |= ISR_NOISE_REDUCTION;
+    if(zigbee_rpm ==0){
+        isr_flags |= ISR_NOISE_REDUCTION;
+    }
 }
 
