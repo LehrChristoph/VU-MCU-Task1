cd68d6d969d953926b72cc474b18bcd2640360cf
christophLehr <christoph.lehr@gmx.at>
2018-10-25 18:50:00 +0200

    added init stuff to main

--- Application/src/main.c
+++ Application/src/main.c
@@ -7,8 +7,17 @@
 
 #include <avr/io.h>
 
+#include "application/game.h"
+
+void init_application(void);
+
 int main(int argc, char const *argv[])
 {
+    init_application();
+}
 
 
+void init_application(void)
+{
+    game_init();
 }
