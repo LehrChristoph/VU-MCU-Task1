208efc65664dd47f23e0bf5999d5ef1f1197511f
Christoph Lehr <christoph@localhost.localdomain>
2018-11-04 11:22:36 +0100

    extended defines and added enum for display state

--- Application/src/application/field.h
+++ Application/src/application/field.h
@@ -27,12 +27,40 @@
     #define FIELD_GLCD_HEIGTH 64
 #endif
 
+#ifndef FIELD_STRING_POSITION_Y
+    #define FIELD_STRING_POSITION_Y 32
+#endif
+
+#ifndef FIELD_TITLE_STRING_POSITION_X
+    #define FIELD_TITLE_STRING_POSITION_X 10
+#endif
+
+#ifndef FIELD_GAME_OVER_STRING_POSITION_X
+    #define FIELD_GAME_OVER_STRING_POSITION_X 13
+#endif
+
+#ifndef FIELD_BARRIER_ARRAY_SIZE
+    #define FIELD_BARRIER_ARRAY_SIZE 8
+#endif
+
+#ifndef FIELD_BARRIER_BLOCK_WIDTH
+    #define FIELD_BARRIER_BLOCK_WIDTH 8
+#endif
+
+#ifndef FIELD_BALL_INIT_POSITION
+    #define FIELD_BALL_INIT_POSITION 64
+#endif
 
 typedef struct __field_barrier
 {
     uint16_t seed;
     uint8_t glcd_position;
 } field_barrier;
+
+typedef enum {
+	FIELD_DISPLAY_READY,
+    FIELD_ALREADY_DISPLAYED,
+} display_state_t;
 
 
 void field_init(void);
