aaae138bfe0a416da9c942c55ae1ef1bd30b6024
Christoph Lehr <christoph@t198-056.demo.tuwien.ac.at>
2018-11-20 13:40:34 +0100

    removed commented out stuff

--- Application/src/main.c
+++ Application/src/main.c
@@ -13,22 +13,8 @@
 #include "application/game.h"
 #include "modules/Tasker.h"
 
-#include "application/controls.h"
-
-void main_send_music(void);
-
-
 int main(int argc, char const *argv[])
 {
-
-    PORTH = 0x00;
-    DDRH = 0xFF;
-
-    PORTK = 0x00;
-    DDRK = 0xFF;
-
-    PORTL = 0x00;
-    DDRL = 0xFF;
 
     Tasker_init();
     game_init();
@@ -37,27 +23,12 @@ int main(int argc, char const *argv[])
     sleep_enable();
     game_start();
 
-    uint16_t counter;
-    uint8_t counter_out;
     while(1)
     {
-        // sleep_cpu();
-        // while(mp3Busy() == true);
-        // game_cyclic_check();
-        PORTH ^= (1 << PORTH0);
-        Task_dispatch_tasks();
-        PORTH ^= (1 << PORTH1);
-        counter++;
-        if(counter > 1000)
-        {
-            counter_out++;
-            counter=0;
-            PORTL = counter_out;
-        }
-    }
+        sleep_cpu();
 
-    // sei();
-    // controls_init();
-    // while(1);
+        Task_dispatch_tasks();
+
+    }
 
 }
